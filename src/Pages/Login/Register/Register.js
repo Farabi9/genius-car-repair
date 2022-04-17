bb02c3ff93b95266888765d5f48cb0ab638ab415
Farabi9 <ifatkhan633@gmail.com>
2022-04-17 15:51:16 +0600

    google login done

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -3,6 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import {useCreateUserWithEmailAndPassword} from 'react-firebase-hooks/auth'
 import './Register.css'
 import auth from '../../../firebase.init';
+import SocialLogin from '../SocialLogin/SocialLogin';
 
 const Register = () => {
     const [
@@ -39,10 +40,11 @@ const Register = () => {
                 <br />
                 <input type="password" name="password" id="" placeholder='Password' required />
                 <br />
-                <input type="submit" value="Register" />
+                <input className='bg-dark text-white' type="submit" value="Register" />
 
             </form>
             <p>Already have an accout? <Link to='/login' className='text-danger text-decoration-none' onClick={navigateLogin}>Please Login</Link></p>
+            <SocialLogin></SocialLogin>
         </div>
     );
 };
